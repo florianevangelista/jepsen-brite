30355b862b643351f62f1b7d3bbdd7a0469f033d
florianevangelista <florian.evangelista0@gmail.com>
2020-07-24 14:35:10 +0200

    Add files on florian

--- pages/validationCreatEvent.php
+++ pages/validationCreatEvent.php
@@ -1,18 +1,12 @@
 <?php
-
-try
-{
-	$bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-}
-catch (Exception $e)
-{
+    try {
+        $bdd = new PDO('mysql:host=localhost;dbname=event_manager;charset=utf8', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    } catch (Exception $e) {
         die('Erreur : ' . $e->getMessage());
-}
+    }
 
-$requete = $bdd->prepare('INSERT INTO event(autor, category, message) VALUES(?, ?)'); 
+    $requete = $bdd->prepare('INSERT INTO evenements (Title, dt, hr, Img, Dsc, Category, Personid) VALUES(?, ?, ?, ?, ?, ?)'); 
+    $requete->execute(array($_POST['title'], $_POST['start-event'], $_POST['startTime'], $_POST['image'], $_POST['description'], $_POST['event'], 1));
 
-$requete->execute(array($_POST['autor'], $_POST['message']));
-
-
-header('Location: page-creatEvent.html');
+    header('location : page-creatEvent.html');
 ?>

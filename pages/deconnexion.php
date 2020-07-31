f09758d34b7b6a36641222ad9689dd35d2890753
donovanherion <donovanherion@gmail.com>
2020-07-31 15:05:37 +0200

    merged master

--- pages/deconnexion.php
+++ pages/deconnexion.php
@@ -2,11 +2,7 @@
 session_start();
     try
     {
-<<<<<<< HEAD
         $bdd = new PDO('mysql:host=localhost;dbname=event_manager;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-=======
-        $bdd = new PDO("mysql:host=zpfp07ebhm2zgmrm.chr7pe7iynqr.eu-west-1.rds.amazonaws.com;dbname=jmgevcvn8tc1r1u3", 'ppitvzphdz3rrjs4', 'rcsmt0yc25rgs1zj', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
->>>>>>> master
     }
     catch (Exception $e)
     {

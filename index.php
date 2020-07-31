e9695f3058dea40c15c3f121a90c4024ba100c30
donovanherion <donovanherion@gmail.com>
2020-07-31 16:05:08 +0200

    local

--- index.php
+++ index.php
@@ -103,7 +103,7 @@ $Parsedown = new Parsedown();
 
 <?php 
 
-$bdd = new PDO("mysql:host=zpfp07ebhm2zgmrm.chr7pe7iynqr.eu-west-1.rds.amazonaws.com;dbname=jmgevcvn8tc1r1u3", 'ppitvzphdz3rrjs4', 'rcsmt0yc25rgs1zj', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+$bdd = new PDO('mysql:host=localhost;dbname=event_manager;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 
 
 if (isset($_POST['category'])) { $ChosenCategory = $_POST['category'];}

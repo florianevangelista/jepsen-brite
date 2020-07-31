241ed9021c497a662eb6317ed6f8941f69433640
florianevangelista <florian.evangelista0@gmail.com>
2020-07-31 14:14:32 +0200

    Modification of mail.php

--- pages/mail.php
+++ pages/mail.php
@@ -16,7 +16,7 @@
 
     try {
         //Server settings
-        $mail->SMTPDebug = 2;                      // Enable verbose debug output
+        $mail->SMTPDebug = 0;                      // Enable verbose debug output
         $mail->isSMTP();                                            // Send using SMTP
         $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   // Enable SMTP authentication

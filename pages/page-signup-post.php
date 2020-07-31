a63258d4a30d6889d52d1f3b9893c34854d93ce6
florianevangelista <florian.evangelista0@gmail.com>
2020-07-31 15:23:31 +0200

    Modification of dpo

--- pages/page-signup-post.php
+++ pages/page-signup-post.php
@@ -2,7 +2,7 @@
 
 try
 {
-    $bdd = new PDO("mysql:host=zpfp07ebhm2zgmrm.chr7pe7iynqr.eu-west-1.rds.amazonaws.com;dbname=jmgevcvn8tc1r1u3", 'ppitvzphdz3rrjs4', 'rcsmt0yc25rgs1zj', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    $bdd = new PDO("mysql:host=zpfp07ebhm2zgmrm.chr7pe7iynqr.eu-west-1.rds.amazonaws.com;dbname=iaj0d3bfcqdzn9jm", 'pec75srf9evxqr4q', 'vaaj2gywif3r1p6h', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 
 }
 catch (Exception $e)

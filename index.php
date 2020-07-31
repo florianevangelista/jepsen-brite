c5189f973c7a38fab71a499212d3ee404a08616b
florianevangelista <florian.evangelista0@gmail.com>
2020-07-31 14:09:08 +0200

    Push Florian on master

--- index.php
+++ index.php
@@ -103,7 +103,7 @@ $Parsedown = new Parsedown();
 
 <?php 
 
-$bdd = new PDO("mysql:host=localhost;dbname=event_manager;charset=utf8", "root", "");
+$bdd = new PDO("mysql:host=zpfp07ebhm2zgmrm.chr7pe7iynqr.eu-west-1.rds.amazonaws.com;dbname=jmgevcvn8tc1r1u3", 'ppitvzphdz3rrjs4', 'rcsmt0yc25rgs1zj', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 
 
 if (isset($_POST['category'])) { $ChosenCategory = $_POST['category'];}
@@ -325,8 +325,51 @@ INNER JOIN persons p ON c.person_id = p.Personid ORDER BY id DESC");
             </div>
         </section>
 
+        <!-- Footer Start -->
+        <footer class="footer">
+            <div class="container">
+                <div class="row d-flex justify-content-around">
+                    <div class="col-lg-4 col-12 mb-0 mb-md-4 pb-0 pb-md-2">
+                        <a class="logo-footer" href="../index.php">Jepsen-brite<span class="text-primary">.</span></a>
+                        <p class="mt-4">Start working with Jepsen-brite.</p>
+                        <ul class="list-unstyled social-icon social mb-0 mt-4">
+                            <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-facebook" title="Facebook"></i></a></li>
+                            <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-instagram" title="Instagram"></i></a></li>
+                            <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-twitter" title="Twitter"></i></a></li>
+                        </ul><!--end icon-->
+                    </div><!--end col-->
+                    
+                    <div class="col-lg-2 col-md-4 col-12 mt-4 mt-sm-0 pt-2 pt-sm-0">
+                        <h4 class="text-light footer-head">Quick link</h4>
+                        <ul class="list-unstyled footer-list mt-4">
+                            <li><a href="pages/page-creatEvent.php" class="text-foot"><i class="mdi mdi-chevron-right mr-1"></i>Event</a></li>
+                            <li><a href="pages/page-login.php" class="text-foot"><i class="mdi mdi-chevron-right mr-1"></i>Login</a></li>
+                            <li><a href="pages/page-signup.php" class="text-foot"><i class="mdi mdi-chevron-right mr-1"></i>Signup</a></li>
+                            <li><a href="pages/profilValider.php" class="text-foot"><i class="mdi mdi-chevron-right mr-1"></i>Profil</a></li>
+                        </ul>
+                    </div><!--end col-->
 
-        <?php include 'pages/footer.php';?>
+                    <div class="col-lg-3 col-md-4 col-12 mt-4 mt-sm-0 pt-2 pt-sm-0">
+                        <h4 class="text-light footer-head">Newsletter</h4>
+                        <p class="mt-4">Sign up and receive the latest tips via email.</p>
+                        <form>
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    <div class="foot-subscribe form-group position-relative">
+                                        <label>Write your email <span class="text-danger">*</span></label>
+                                        <i class="mdi mdi-email ml-3 icons"></i>
+                                        <input type="email" name="email" id="emailsubscribe" class="form-control pl-5 rounded" placeholder="Your email : " required>
+                                    </div>
+                                </div>
+                                <div class="col-lg-12">
+                                    <input type="submit" id="submitsubscribe" name="send" class="btn btn-primary rounded w-100" value="Subscribe">
+                                </div>
+                            </div>
+                        </form>
+                    </div><!--end col-->
+                </div><!--end row-->
+            </div><!--end container-->
+        </footer><!--end footer-->
 
         <!-- javascript -->
         <script src="js/jquery.min.js"></script>
--- pages/deleteProfile.php
+++ pages/deleteProfile.php
@@ -2,7 +2,7 @@
 session_start();
     try
     {
-        $bdd = new PDO('mysql:host=localhost;dbname=jepsenBrite;charset=utf8', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+        $bdd = new PDO("mysql:host=zpfp07ebhm2zgmrm.chr7pe7iynqr.eu-west-1.rds.amazonaws.com;dbname=jmgevcvn8tc1r1u3", 'ppitvzphdz3rrjs4', 'rcsmt0yc25rgs1zj', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
     }
     catch (Exception $e)
     {
